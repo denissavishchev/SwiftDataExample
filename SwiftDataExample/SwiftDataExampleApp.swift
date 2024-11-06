57f975cc3d2da3dbef30e3802a959616ae566455
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-06 21:05:11 +0100

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Devis on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
